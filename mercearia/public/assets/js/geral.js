64d450469a4f1262549739124d14f0c90346106c
oliviorui <oliviorui@gmail.com>
2026-02-03 10:50:07 +0200

    Reestruturação do repositório

--- mercearia/public/assets/js/geral.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".main-content").css("display", "none");
-
-    $(".main-content").fadeIn(900);
-
-    $("a").click(function(event) {
-        $("body").fadeOut(900);
-    });
-});
